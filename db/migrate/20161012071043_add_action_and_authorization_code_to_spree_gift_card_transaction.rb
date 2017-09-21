a9b9240af4b08d4d347b4c9999c142aeb2629f9e
Gonzalo Moreno <gmoreno@acid.cl>
2017-09-21 14:28:40 -0300

    final upgrade to work with spree 3-3

--- db/migrate/20161012071043_add_action_and_authorization_code_to_spree_gift_card_transaction.rb
+++ db/migrate/20161012071043_add_action_and_authorization_code_to_spree_gift_card_transaction.rb
@@ -1,4 +1,4 @@
-class AddActionAndAuthorizationCodeToSpreeGiftCardTransaction < ActiveRecord::Migration
+class AddActionAndAuthorizationCodeToSpreeGiftCardTransaction < ActiveRecord::Migration[5.1]
   def change
     add_column :spree_gift_card_transactions, :action, :string
     add_column :spree_gift_card_transactions, :authorization_code, :string
